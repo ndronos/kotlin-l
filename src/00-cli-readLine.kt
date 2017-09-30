edc610f126cee911894e4ebed7110cc953da091c
Leandro Leite <ndronos@gmail.com>
2017-09-29 23:43:31 -0300

    Code Lesson CLI readLine()

--- src/00-cli-readLine.kt
+++ src/00-cli-readLine.kt
@@ -8,10 +8,19 @@ fun main(args: Array<String>) {
 
     print("Enter name: ")
     var name = readLine()
+    print("Enter age: ")
+    var age = readLine()
+    print("Enter sector: ")
+    var sector = readLine()
+    print("Enter force: ")
+    var force = readLine()
 
 
     println("*** Output ***")
 
     println("name: " + name)
+    println("age: " + age)
+    println("sector: " + sector)
+    println("force: " + force)
 
 }
