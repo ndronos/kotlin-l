ffe247b4c1ab103155b6353dfdae8676c3680e0e
Leandro Leite <ndronos@gmail.com>
2017-09-30 00:44:07 -0300

    Note Lesson comments

--- src/00-helloPackage.kt
+++ src/00-helloPackage.kt
@@ -1,7 +1,7 @@
-/* /index naguib-2-11
- *  package println() main()       this is a multiline comment
+/* /index naguib-2-11 alrubaye-2-11
+ *  package println() main()       this is a multiline comment, slash star
  */
-                                // this is a singleline comment
+                                // this is a singleline comment, double slash
 package hello                   // optional package header
                                 // package-level function, which takes
 fun main(args: Array<String>) { // an Array of strings as a parameter
