774653dd56867f0fdc8a881a82cc585eff71b74b
Leandro Leite <ndronos@gmail.com>
2017-09-29 21:55:46 -0300

    Code var val datatypes

---
+++ src/01-datatypesVarVal.kt
@@ -0,0 +1,25 @@
+/* alrubaye-2-9
+ * var val datatypes concatenation
+ * var - read, write; Mutable, can be reassined
+ * val - read only, Immutable, define 1st time and will never change
+ */
+
+fun main(args: Array<String>) {
+
+    val name = "Kotlin"
+    val age =  7
+
+    println("Name: " + name)
+    println("Age: " + age)
+
+    val pi:Double = 3.1415
+    println("Pi: "+ pi)
+
+    var dep:String?
+    dep = "Software Engineering"
+    println("Department: " + dep)
+
+    dep = "JetBrains"
+    println("Made by " + dep)
+
+}
