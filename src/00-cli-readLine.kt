692e7a4d9159a471c37526306bfc915b244980d8
Leandro Leite <ndronos@gmail.com>
2017-09-29 23:37:45 -0300

    Code Lesson CLI readLine()

---
+++ src/00-cli-readLine.kt
@@ -0,0 +1,17 @@
+/* alrubaye-2-10
+ * readLine() println() print()
+ */
+
+fun main(args: Array<String>) {
+
+    println("*** Input ***")
+
+    print("Enter name: ")
+    var name = readLine()
+
+
+    println("*** Output ***")
+
+    println("name: " + name)
+
+}
